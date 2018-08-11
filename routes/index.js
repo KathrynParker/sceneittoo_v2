7e8cdc6b08f6b844e0eb4517e32f63bb255c4714
Kathryn Parker <kparker@warpedworks.com>
2018-08-10 20:50:43 -0400

    connect DB

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const dotenv = require('dotenv');
+
+dotenv.load();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
